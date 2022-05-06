efc25aea0d41ebf62eeeb2627e2cb0676bc98117
Teddy <teddy.francfort@gmail.com>
2022-05-06 15:15:07 +0400

    Extract tick logic for a normal Item in his tick() method. SpecialItem created to ease the refactoring keeping the default logic for special items. GildedRose decide wether to create a normal or special item based on the given name.

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use App\Models\Item;
+use App\Models\SpecialItem;
 
 class GildedRose
 {
     public static function of($name, $quality, $sellIn) : Item {
-        return new Item($name, $quality, $sellIn);
+        return match ($name) {
+            'Aged Brie',
+            'Backstage passes to a TAFKAL80ETC concert',
+            'Sulfuras, Hand of Ragnaros' => new SpecialItem($name, $quality, $sellIn),
+            default => new Item($name, $quality, $sellIn),
+        };
     }
 }
--- src/Models/Item.php
+++ src/Models/Item.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
-class Item
+class Item implements ItemInterface
 {
-    use Tickable;
-
     public function __construct(public string $name, public int $quality, public int $sellIn)
     {
+    }
+
+    public function tick(): void
+    {
+        if ($this->quality > 0) {
+            $this->quality = $this->quality - 1;
+        }
+
+        $this->sellIn = $this->sellIn - 1;
+
+        if ($this->sellIn < 0 && $this->quality > 0) {
+            $this->quality = $this->quality - 1;
+        }
     }
 }
