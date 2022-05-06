5797b0f036c087bc92ed8b75eb1aac23042485ce
Teddy <teddy.francfort@gmail.com>
2022-05-06 18:25:04 +0400

    Delete useless calculation in BackstageItem.php when quality drop to 0 after the concert

--- src/Models/BackstageItem.php
+++ src/Models/BackstageItem.php
@@ -26,7 +26,7 @@ class BackstageItem extends Item
         $this->sellIn = $this->sellIn - 1;
 
         if ($this->sellIn < 0) {
-            $this->quality = $this->quality - $this->quality;
+            $this->quality = 0;
         }
     }
 }
