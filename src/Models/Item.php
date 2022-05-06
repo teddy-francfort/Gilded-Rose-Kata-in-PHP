24ba19df809ed6d3900bd188e8a7de05844bb36d
Teddy <teddy.francfort@gmail.com>
2022-05-06 17:41:14 +0400

    const LOWER_QUALITY_BY added to the base Item. It allows to change the decreasing rate in sub items.

--- src/Models/Item.php
+++ src/Models/Item.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 class Item implements ItemInterface
 {
+    public const LOWER_QUALITY_BY = 1;
+
     public function __construct(public string $name, public int $quality, public int $sellIn)
     {
     }
@@ -11,13 +13,13 @@ class Item implements ItemInterface
     public function tick(): void
     {
         if ($this->quality > 0) {
-            $this->quality = $this->quality - 1;
+            $this->quality = $this->quality - static::LOWER_QUALITY_BY;
         }
 
         $this->sellIn = $this->sellIn - 1;
 
         if ($this->sellIn < 0 && $this->quality > 0) {
-            $this->quality = $this->quality - 1;
+            $this->quality = $this->quality - static::LOWER_QUALITY_BY;
         }
     }
 }
